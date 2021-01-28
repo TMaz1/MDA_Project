41ab6f87ef769950ec1f14a0499b0d48e8776714
TayyabaM <56393095+TayyabaM@users.noreply.github.com>
2021-01-28 15:12:38 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+//class component displaying string to app
+class HelloWorldApp extends Component {
+  render(){
+    return (
+        <View>
+          <Text>Hello World!</Text>
+        </View>
+    );
+  }
+}
+
+export default HelloWorldApp
