4c99ca6cd0d7ff351d009384a166c76c983243bb
TayyabaM <56393095+TayyabaM@users.noreply.github.com>
2021-02-11 14:33:28 +0000

    Update App.js
    Added a temporary introductory screen after logging in: shows a list of all coffee location names.

--- App.js
+++ App.js
@@ -1,15 +1,96 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View, Text, ToastAndroid, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-//class component displaying string to app
-class HelloWorldApp extends Component {
-  render(){
-    return (
-        <View>
-          <Text>Hello World!</Text>
+//home screen displaying all the coffee location names in the database
+class HomeScreen extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      listData: []
+    }
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.props.navigation.addListener('focus', () => {
+      this.checkLoggedIn();
+    });
+
+    this.getData();
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  getData = async () => {
+    const value = await AsyncStorage.getItem('@session_token'); //REPETITION!!!!
+    return fetch("http://10.0.2.2:3333/api/1.0.0/find", {
+      'headers': {
+        'X-Authorization': value
+      }
+    })
+    .then((response) => {
+      if(response.status === 200){
+        return response.json()
+      }else if (response.status === 401){
+        ToastAndroid.show("You're not logged in", ToastAndroid.SHORT)
+        this.props.navigation.navigate("Login");
+      }else{
+        throw 'Something went wrong';
+      }
+    })
+    .then((responseJson) => {
+      this.setState({
+        isLoading: false,
+        listData: responseJson
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+      ToastAndroid.show(error, ToastAndroid.SHORT);
+    })
+  }
+
+  checkLoggedIn = async () => {
+    const value = await AsyncStorage.getItem('@session_token'); //REPETITION!!!!
+    if (value == null) {
+      this.props.navigation.navigate('Login');
+    }
+  };
+
+  render() {
+
+    if (this.state.isLoading){
+      return (
+        <View
+          style = {{
+            flex: 1, 
+            flexDirection: 'column', 
+            justifyContent: 'center', 
+            alignItems: 'center',
+          }}>
+          <Text>Loading...</Text>
         </View>
-    );
+      );
+    }else{
+      return (
+        <View>
+          <FlatList
+            data = {this.state.listData}
+            renderItem = {({item}) => (
+              <View>
+                <Text>{item.location_name}</Text>
+              </View>
+            )}
+            keyExtractor = {(item, index) => item.location_id.toString()}
+          />
+        </View>
+      );
+    }
   }
 }
 
-export default HelloWorldApp
+export default HomeScreen;
